81a8e87d1a4cf17ac5d8fa695cabb3f6890e97ac
larionov24e@gmail.com <larionov24e@gmail.com>
2020-04-09 19:33:01 +0300

    init

---
+++ LoggerBundle.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace CashierLogger\LoggerBundle;
+
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoggerBundle
+{
+    public function test()
+    {
+        $connection = new AMQPStreamConnection('rabbitmq', 5672, 'rabbitmq', 'rabbitmq');
+        $channel = $connection->channel();
+
+        $channel->queue_declare('log', false, true, false, false);
+
+        $msg = new AMQPMessage('Hello World!',
+            ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]
+        );
+
+        $channel->basic_publish($msg, '', 'log');
+
+        $channel->close();
+        $connection->close();
+
+        return new Response('Sent message');
+    }
+}
