75e3f16dfa039469e87c638f71a7866758eb540e
larionov24e@gmail.com <larionov24e@gmail.com>
2020-04-09 21:11:03 +0300

    delte idea

---
+++ Logger.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace CashierLogger\LoggerBundle;
+
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class Logger
+{
+    public function test()
+    {
+        $connection = new AMQPStreamConnection('rabbitmq', 5672, 'rabbitmq', 'rabbitmq');
+        $channel = $connection->channel();
+
+        $channel->queue_declare('log', false, true, false, false);
+
+        $msg = new AMQPMessage('Hello World!',
+            ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]
+        );
+
+        $channel->basic_publish($msg, '', 'log');
+
+        $channel->close();
+        $connection->close();
+
+        return true;
+    }
+}
--- LoggerBundle.php
+++ LoggerBundle.php
@@ -3,27 +3,8 @@
 
 namespace CashierLogger\LoggerBundle;
 
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-use PhpAmqpLib\Connection\AMQPStreamConnection;
-use PhpAmqpLib\Message\AMQPMessage;
-use Symfony\Component\HttpFoundation\Response;
-
-class LoggerBundle
+class LoggerBundle extends Bundle
 {
-    public function test()
-    {
-        $connection = new AMQPStreamConnection('rabbitmq', 5672, 'rabbitmq', 'rabbitmq');
-        $channel = $connection->channel();
-
-        $channel->queue_declare('log', false, true, false, false);
-
-        $msg = new AMQPMessage('Hello World!',
-            ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]
-        );
-
-        $channel->basic_publish($msg, '', 'log');
-
-        $channel->close();
-        $connection->close();
-    }
 }
